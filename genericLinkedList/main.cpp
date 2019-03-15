5d3d363df33943d7c9b4576b2d867c567fb3c6bb
Asaf <asaf4me@gmail.com>
2019-03-15 16:45:36 +0200

    First

---
+++ genericLinkedList/main.cpp
@@ -0,0 +1,13 @@
+/*
+*   Main file for checking the generic linked list librery
+*   All right reserved to @Asaf Joseph
+*   Free for USE
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char* argv[]){
+    cout << "hello world" << endl;
+    return EXIT_SUCCESS;
+}
