0cf6f9b6ab8b9ab2755362dbea161a3abec3b743
asaf4me <38263049+asaf4me@users.noreply.github.com>
2019-04-08 09:07:14 +0300

    Update rm.c

--- securenBin/rm.c
+++ securenBin/rm.c
@@ -5,10 +5,10 @@
 #include <linux/limits.h>
 #include <stdlib.h>
 
-#define LENGTH 39
+#define LENGTH 38
 
 const char* const blackList[] = {"/", "/aquota.group", "/aquota.user", "/backup", "/backups-test", "/bash", "/boot", "/daniel", "/dev",
-										"/dumps", "/eli_install", "/error_log", "/etc", "/home", "/home2", "/idan", "/iscsi", "/logs", "/media",
+										"/dumps", "/eli_install", "/error_log", "/etc", "/home", "/home2", "/idan", "/iscsi", "/media",
 										"/mnt", "/ofir", "/opt", "/proc", "/quota.group", "/quota.user", "/razor-agent.log", "/root", "/run", "/srv",
 										"/svvedice.tar.gz", "/sys", "/tmp", "/usr", "/usr/bin", "/usr/lib", "/usr/lib64", "/usr/local/cpanel/scripts",
 										"/usr/sbin", "/var"};
@@ -50,7 +50,7 @@ int recursivePermission(char *param){
 	{
 		ptr = strchr(++ptr,'/');
 		if(ptr == NULL)
-			return -1;
+				return -1;
 	}
 	return 1;
 }
@@ -58,32 +58,32 @@ int recursivePermission(char *param){
 // Validating that you can do the command on this argv
 int validatePath(int argc, char* argv[]){
 	char absulote[PATH_MAX];
+	char *temp[argc];
 	for(int i = 1 ; i < argc ; i++)
 	{
 		// Get the absolute path instead relative one
-		argv[i] = realpath(argv[i], absulote);
-		if (argv[i] == NULL)
+		temp[i] = realpath(argv[i], absulote);
+		if (temp[i] == NULL)
 			continue;
 		// Searching for argv[i] in the blacklist
-		if(binarySearch(argv[i]) == 1)
+		if(binarySearch(temp[i]) == 1)
 		{
-			printf("%s %s Permission denied\n", argv[0], argv[i]);
+			printf("%s %s Permission denied\n", argv[0], temp[i]);
 			return -1;
 		}
 		// Searching for blacklist folders in depth 1
-		for(int j = 0 ; j < LENGTH ; j++)
+		for(int j = 1 ; j < LENGTH ; j++)
 		{
-			if(startsWith(argv[i], blackList[j]) == 1)
+			if(startsWith(temp[i], blackList[j]) == 1)
 			{
-				if(recursivePermission(++argv[i]) == -1)
+				if(recursivePermission(++temp[i]) == -1)
 				{
-					printf("%s %s Permission denied\n", argv[0], --argv[i]);
+					printf("%s %s Permission denied\n", argv[0], --temp[i]);
 					return -1;
 				}
-				--argv[i];
+				--temp[i];
 			}
 		}
-		printf("%s\n", argv[i]);
 	}
 	return 1;
 }
@@ -95,6 +95,7 @@ int main(int argc, char* argv[])
 		if(validatePath(argc, argv) == -1)
 			return -1;
 	}
+	return 1;
 	// Plug and play, just change the core command
 	execvp("/usr/bin/jet/rm", argv);
 	return 1;
